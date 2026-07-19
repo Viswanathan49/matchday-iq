cb0f314afd37e1acb2d878f203bc530afb024ea0
Viswanathan49 <118430061+Viswanathan49@users.noreply.github.com>
2026-07-19 19:30:55 +0530

    feat: replace tab favicon with a football SVG logo

--- vite.config.js
+++ vite.config.js
@@ -18,12 +18,12 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/vite.svg',
+            src: '/favicon.svg',
             sizes: '192x192',
             type: 'image/svg+xml'
           },
           {
-            src: '/vite.svg',
+            src: '/favicon.svg',
             sizes: '512x512',
             type: 'image/svg+xml'
           }
